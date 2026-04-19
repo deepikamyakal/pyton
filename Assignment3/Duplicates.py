2620337a8fff87bd82f87d5742c33928b3e61e05
deepikamyakal <deepikamyakal10@gmail.com>
2026-04-19 20:17:44 +0530

    Fixed submodule issue

---
+++ Assignment3/Duplicates.py
@@ -0,0 +1,6 @@
+# 4)Write a program to remove duplicate values from a list using a set.
+
+value = [1,1,1,2,2,3,4,4,5,6]
+
+duplicate = list(set(value))
+print(duplicate)
---
+++ Assignment4/Arithmatic.py
@@ -0,0 +1,12 @@
+# 1.Write a program to demonstrate all arithmetic operators using two user-defined numbers.
+
+a = int(input("Enter first no :"))
+b = int(input("Enter second no :"))
+
+print("Addition :", a + b)
+print("Substraction :", a - b)
+print("Multiplication :", a * b)
+print("Divison :", a / b)
+print("Modulus :", a % b)
+print("Exponent :", a ** b)
+print("Floor Divison:", a // b)
