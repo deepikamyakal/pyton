7cd5578a5ef079ffbb7aae655c60914768c491fa
deepikamyakal <deepikamyakal10@gmail.com>
2026-05-19 19:37:13 +0530

    Day26

---
+++ Day27/Modules.py
@@ -0,0 +1,14 @@
+# libraries ---->packages(folders) ---> modules(files)--->classes---> methods and attributes-->attributes(logic)
+
+# modules in Python : module is python file(.py) which contains classes , methods , variables that can be used in another program or module 
+
+class Sample:
+    name = "Deepika"
+    def demo(self):
+        print(name)
+
+name ="rocky"
+print(name)
+
+obj = Sample()
+obj.demo()
